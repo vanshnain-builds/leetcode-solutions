b9da0b74e59059a3c4ec6813ff71597a65766cfd
Vansh Nain <vanshnain07.ai@gmail.com>
2026-08-20 20:38:01 +0530

    Time: 7 ms (8.12%), Space: 14.6 MB (7.28%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> s(nums.begin(), nums.end());
+        if (s.size()<3 ) return *s.rbegin();
+        else{
+            auto it = s.rbegin();
+            advance(it,2);
+            return *it;
+        }
+    }
+};
