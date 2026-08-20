a76c69aebcc68e7cf57bd0b4727a813dad9f6b11
Vansh Nain <vanshnain07.ai@gmail.com>
2026-08-20 19:45:39 +0530

    Time: 8 ms (8.96%), Space: 8.5 MB (64.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long temp = x;
+        if (temp<0) return false;
+        else{
+                long long rev =0;
+                while (temp != 0){
+                    rev = rev*10 + (temp%10);
+                    temp = floor(temp/10);
+                }
+                if (rev > INT_MAX || rev < INT_MIN) return false;
+                if (rev == x) return true;
+                else return false;
+        }
+    }
+};
