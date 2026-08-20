0580e87708c98f58882a43584495b40bd4ea57a8
Vansh Nain <vanshnain07.ai@gmail.com>
2026-08-21 04:02:34 +0530

    Time: 2 ms (77.46%), Space: 7.8 MB (65.72%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -4,8 +4,15 @@ public:
         if (n > 30 || n<0) return 0;
         else{
             if (n==0) return 0;
-            else if(n==1) return 1;
-            else return fib(n-1) + fib(n-2);
+            if (n==1) return 1;
+            int prev2 = 0;
+            int prev = 1;
+            for (int i = 2; i <= n; i++) {
+                int current = prev + prev2;
+                prev2 = prev;
+                prev = current;
+            }
+            return prev;
         }
     }
 };
