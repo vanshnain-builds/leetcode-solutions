30c9ecfade4f5006f3ffe7bcb02a4c96bf237f6a
Vansh Nain <vanshnain07.ai@gmail.com>
2026-09-10 17:21:17 +0530

    Time: 31 ms (5.16%), Space: 8.6 MB (12.96%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int digit(int n){
+        int count=0;
+        while(n>0){
+            count++;
+            n=n/10;
+        }
+        return count;
+    }
+    int commas(int n){
+        int no_digit = digit(n);
+        if (no_digit%3==0) return (no_digit/3)-1;
+        else return (no_digit/3);
+    }
+    int countCommas(int n) {
+        int count=0;
+        for (int i=1; i<n+1; i++){
+            count += commas(i);
+        }
+        return count;
+    }
+};
